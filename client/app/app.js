3f9a736a4cc41d4d8b61bb93392c1862a6b6f6aa
Tamara Temple <tamouse@gmail.com>
2015-07-20 12:00:37 -0500

    My work on step 2

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,24 @@
 /*
- * TODO: import angular and other dependencies
+ * DONE: import angular and other dependencies
  * like our app.styl and normalize.css and components/home
  * ui-router
  * so the below code works
  */
 
+// NOTE: WTF: normalize.css is a **NODE_MODULE** not a file!
+import 'normalize.css';
+import './app.styl';
+//import './components/home/home.styl';
+
+
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import {home} from './components/home/home'
+
 angular.module('app', [
   // TODO: register other modules here.
   // remember, this array takes strings
   // not objects
+  uiRouter,
+  home.name
 ]);
