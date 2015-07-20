0cef7b46b87670550f955e43ff8707b2fd3d12fb
Tamara Temple <tamouse@gmail.com>
2015-07-20 14:22:57 -0500

    Step 3 work

--- client/app/components/blog/blog.controller.js
+++ client/app/components/blog/blog.controller.js
@@ -1,3 +1,17 @@
-// TODO: create the blog controller
+// DONE: create the blog controller
 // check blog.html for hints on what the controller
 // should have/do
+import _ from 'lodash';
+
+export class BlogController {
+  constructor() {
+    this.message = 'Posts'
+    this.posts = _.times(7, i => {
+      return {
+        title: `Post ${i}`,
+        author: 'Zaphrod Beeblebrox',
+        body: 'Lorem ipsum'
+      }
+    });
+  }
+}
--- client/app/components/blog/blog.js
+++ client/app/components/blog/blog.js
@@ -1,1 +1,13 @@
-// TODO: register your routes for the blog and the directive
+// DONE: register your routes for the blog and the directive
+import angular from 'angular';
+import {blogDirective} from './blog.directive.js';
+
+export const blog = angular.module('blog', [])
+  .config(function($stateProvider) {
+    "use strict";
+    $stateProvider.state('blog', {
+      url: '/blog',
+      template: '<blog></blog>'
+    })
+  })
+  .directive('blog', blogDirective);
